07169df92d1b5c30650b6120e66fedd9a7b1cc52
Ana Cascante <91411170+AnaCascante@users.noreply.github.com>
2024-07-24 12:48:12 +0200

    fixed error

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from '../../assets/react.svg'
 import eazyshop from '../../assets/eazyshop.svg'
-import viteLogo from '../../assets/vite.svg'
 import '../../index.css'
 
 function App() {
@@ -11,12 +9,6 @@ function App() {
     <>
       <div>
         <h1 className="text-3xl font-bold underline">Hello world</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
         <img src={eazyshop} className="logo eazyshop" alt="eazyshop logo" />
       </div>
       <h1>Vite + React</h1>
