a7831531127c017a1795450dbdce17f1f2bbc985
Ana Cascante <91411170+AnaCascante@users.noreply.github.com>
2024-07-24 10:25:34 +0200

    adds logo and pages to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import eazyshop from './assets/eazyshop.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -16,6 +17,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <img src={eazyshop} className="logo eazyshop" alt="eazyshop logo" />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
