e974f26690279d781a07dfec72dc992f747f9195
Ana Cascante <91411170+AnaCascante@users.noreply.github.com>
2024-07-25 09:30:41 +0200

    adds header and footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import eazyshop from '../../assets/eazyshop.svg'
-import '../../index.css'
-import Layout from '../Layout'
+import { createBrowserRouter } from "react-router-dom";
+import { Route, createRoutesFromElements, RouterProvider } from "react-router-dom";
 
+// import pages 
+import RootLayout from "../Layout";
+
+// import components
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+    
+      
+    </Route>
+  )
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world</h1>
-        <img src={eazyshop} className="logo eazyshop" alt="eazyshop logo" />
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Layout />
-    </>
-  )
+    <RouterProvider router={router}>
+      
+    </RouterProvider>
+  );
 }
 
-export default App
+export default App;
