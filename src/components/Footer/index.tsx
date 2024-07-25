8b109f56aa40bba491f80f0d5de74849b7d71c05
Ana Cascante <91411170+AnaCascante@users.noreply.github.com>
2024-07-25 11:44:24 +0200

    fixed responsiveness navbar and footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
     return (
-      <footer className="fixed bottom-0 left-0 right-0 flex flex-row bg-slate-200 justify-center">
+      <footer className="fixed bottom-0 left-0 right-0 flex flex-row flex-grow bg-slate-200 justify-center">
         <p>Copyright &copy; 2024 Ana Cascante</p>
       </footer>
     );
