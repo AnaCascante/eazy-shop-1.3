7ae5f6046da8b3f9d137d3b65fdf60163515f2e0
Ana Cascante <91411170+AnaCascante@users.noreply.github.com>
2024-07-27 14:50:05 +0200

    adds searchbar to the navbar- missing functionality

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import NavBar from "../NavBar";
 
 
+
 export default function Header () {
     return (
-        <header>
+        <header className="fixed top-0 left-0 right-0 z-10 flex flex-grow justify-between items-center bg-slate-200 px-4 md:px-8">
         <>   
         <NavBar/>
-        
         </>
         </header>
     )
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
 import eazyshop from "../../assets/eazyshop.svg";
+import SearchBar from "../SearchBar";
 
 
 
@@ -11,8 +12,10 @@ function NavBar() {
     <nav className="fixed top-0 left-0 right-0 z-10 flex flex-grow justify-between items-center bg-slate-200 px-4 md:px-8">
       <div className="flex items-center space-x-4">
         <img src={eazyshop} alt="EazyShop Logo" className="h-8 mr-3" /> 
-         <NavLink to="/" className="hover:text-pink-500 m-3 ">Home</NavLink>
+        <SearchBar />
+         <NavLink to="/" className="hover:text-pink-500 m-3">Home</NavLink>
          <NavLink to="/ContactPage" className="hover:text-pink-500 m-3">Contact</NavLink>
+         
       </div>
       <div className="flex items-center space-x-4 mr-4">
          <NavLink to="/CheckoutPage" className="hover:text-pink-500 m-0.5"><FaShoppingCart /></NavLink>
