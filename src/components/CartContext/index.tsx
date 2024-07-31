0076577a72234df48c5f433ff2ac275cceb4d5bc
Ana Cascante <91411170+AnaCascante@users.noreply.github.com>
2024-07-31 14:18:28 +0200

    adds useCart hook

--- src/components/CartContext/index.tsx
+++ src/components/CartContext/index.tsx
@@ -44,3 +44,11 @@ export const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
     </CartContext.Provider>
   );
 };
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};
