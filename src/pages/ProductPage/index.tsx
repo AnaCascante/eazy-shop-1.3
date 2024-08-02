81e01e65d8fdb22e8bcf77b7e5f6779b9029c709
Ana Cascante <91411170+AnaCascante@users.noreply.github.com>
2024-08-02 16:17:15 +0200

    removes unused import

--- src/pages/ProductPage/index.tsx
+++ src/pages/ProductPage/index.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useCart } from "../../hooks/useCart";
-import { ProductComponent } from "../../components/Product";
+
 
 type Image = {
   url: string;
